fa2eba7848655b7fa6cb98009c871296c12888bb
Jeet Nagda <jnagda@eng.ucsd.edu>
2019-03-18 21:53:16 -0700

    init simple flask

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-   return '<html><body><h1>\'Hello World\'</h1></body></html>'
+    return render_template('index.html')
 
-@app.route('/hello/<user>')
-def hello_name(user):
-   return render_template('hello.html', name = user)
+
+@app.route('/first_puzzle')
+def first_puzzle():
+    return render_template('puzzle.html')
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
