92bc6f189c903948c79607870ad0f55b0b7ed323
jeet.nagda <jeet.nagda@workday.com>
2019-03-20 09:11:38 -0700

    bump

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,8 +3,14 @@ from flask import Flask, render_template
 from flaskr import db
 
 
+#
+def page_not_found(e):
+    return render_template('nope.html'), 404
+
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    app.register_error_handler(404, page_not_found)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -24,13 +30,13 @@ def create_app(test_config=None):
     db.init_app(app)
     return app
 
+
 app = create_app()
+
 
 @app.route('/')
 def index():
-    cur = db.get_db().cursor()
     return render_template('index.html')
-
 
 @app.route('/first_puzzle')
 def first_puzzle():
@@ -40,21 +46,10 @@ def first_puzzle():
 
 @app.route('/themetagames')
 @app.route('/themetagame')
+@app.route('/metagame')
+@app.route('/metagames')
 def second_puzzle():
     return render_template('second_puzzle.html')
-
-
-@app.route('/may+15')
-@app.route('/may+16')
-@app.route('/may+19')
-@app.route('/june+17')
-@app.route('/june+18')
-@app.route('/july+14')
-@app.route('/aug+14')
-@app.route('/aug+15')
-@app.route('/aug+17')
-def wrong_date():
-    return render_template('nope.html')
 
 
 @app.route('/july+16')
@@ -64,3 +59,4 @@ def third_puzzle():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    #app.run(debug=True, host= '0.0.0.0') # to run on computers public ip
